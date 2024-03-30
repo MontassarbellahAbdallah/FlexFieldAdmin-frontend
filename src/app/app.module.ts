58213288b10aeb830aed590cef40c5c251caf8c4
Abdallah Montassar_bellah <amontasar2@gmail.com>
2024-03-31 00:34:00 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AjouterAdminComponent } from './ajouter-admin/ajouter-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AjouterAdminComponent
   ],
   imports: [
     BrowserModule,
