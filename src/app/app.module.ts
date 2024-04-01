4c28be09af4d5c77eb63c5bf6249c5b6f9b38d63
Abdallah Montassar_bellah <amontasar2@gmail.com>
2024-04-02 00:23:16 +0200

    integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AjouterAdminComponent } from './ajouter-admin/ajouter-admin.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AjouterAdminComponent
+    AjouterAdminComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
